3a38dae47b6622a2d42a5832d3de54513e5b940d
Matt Zhang <set.stun@gmail.com>
2024-11-05 14:37:08 -0600

    Corrections according to design docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
     content: ["*"],
     theme: {
         extend: {
+            colors: {
+                slate: {
+                    300: 'hsl(212, 45%, 89%)',
+                    500: 'hsl(216, 15%, 48%)',
+                    900: 'hsl(218, 44%, 22%)'
+                }
+            },
             fontFamily: {
                 "outfit": ["Outfit", "sans-serif"],
             },
